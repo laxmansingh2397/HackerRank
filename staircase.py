731bf36928f64f56a298870de6bad9a43dafa892
Laxman Singh <singhlaxman2397@gmail.com>
2025-07-24 13:24:43 +0530

    Solved problem staircase

---
+++ staircase.py
@@ -0,0 +1,13 @@
+# n = 4
+
+def staircase(n):
+    for i in range(n):
+        print(" " * (n - (i + 1)) + "#" * (i + 1))
+
+
+if __name__ == "__main__":
+
+    n = int(input().strip())
+
+    staircase(n)
+    
