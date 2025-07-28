9bb88501ddfb2ba102bee5f78edc27d49e19068f
Laxman Singh <singhlaxman2397@gmail.com>
2025-07-28 15:14:34 +0530

    Solved problem camelcase

---
+++ day-4/camelcase.py
@@ -0,0 +1,19 @@
+# s = oneTwoThree
+
+def camelcase(s):
+
+    count = 1
+    
+    for i in s:
+        if i.isupper():
+            count += 1
+    
+    return count
+
+if __name__ == '__main__':
+    
+    s = input()
+
+    result = camelcase(s)
+
+    print(result)
