aff8679f294d3a27e0e3daacf46a57a2d16afe4b
Laxman Singh <singhlaxman2397@gmail.com>
2025-07-31 11:56:13 +0530

    complete problem pangrams

---
+++ day-7/pangrams.py
@@ -0,0 +1,17 @@
+
+
+def pangrams(s):
+    new_s = "".join(s.lower().split())
+    set_s = set(new_s)
+    if len(set_s) == 26:
+        return "pangram"
+    else:
+        return "not pangram"
+
+if __name__ == '__main__':
+
+    s = input()
+
+    result = pangrams(s)
+
+    print(result)
